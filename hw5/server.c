1f60a47cbcd4861c3a68fc876d45744f82056ba2
SKhan2001 <92888243+SKhan2001@users.noreply.github.com>
2021-12-04 17:05:47 -0500

    Add files via upload

---
+++ hw5/server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+
+int main(int argc, char* argv[]){
+
+
+}
