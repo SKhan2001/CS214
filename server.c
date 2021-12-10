94d1cb1c1033a42eac8cb05034b30056cf9d6e69
SKhan2001 <Sameerk3412@gmail.com>
2021-12-09 21:20:18 -0500

    Updated server.c

--- server.c
+++ server.c
@@ -1,24 +1,30 @@
-
 #include <stdio.h>
-#include <errno.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 #include <netinet/in.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <sys/time.h>
-#define MAX 80
-#define MAX_CLIENTS 4
-#define true 1
-#define false 0
+#include <semaphore.h>
+#include <pthread.h>
+#define MAX 100
+void readInputs(int);
+void* reader(void*);
+void* writer(void*);
 int total_tomatoes =0; 
+sem_t x,y;
+pthread_t tid;
+pthread_t writerthreads[100];
+pthread_t readerthreads[100];
+int readercount = 0;
+
+
 
 struct player{
-  char player_ID[10];
-  int x_position; 
-  int y_position;
+    char player_ID[10];
+    int x_position; 
+    int y_position;
 };
 struct socket_addr  { 
   uint16_t        sin_family;  /* Protocol family (always AF_INET) */ 
@@ -27,35 +33,71 @@ struct socket_addr  {
   unsigned char   sin_zero[8]; /* Pad to sizeof(struct sockaddr) */ 
 }; 
 
-void func(int sockfd)
-{
-  int array[10][10] = {0};
-  int numArr[3][3];
+void* reader(void* param){
 
-        // read the message from client and copy it in buffer
-        read(sockfd, array, sizeof(array));
-        // print buffer which contains the client contents
-        bzero(array, MAX);
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                array[i][j] =1;
-                numArr[i][j] = array[i][j];
-            }
-    
-        }  
-        // and send that buffer to client
-        write(sockfd, numArr, sizeof(numArr));
-   
-        
+  //lock the semaphore
+  sem_wait(&x);
+  readercount++;
+
+  if(readercount ==1)
+    sem_wait(&y);
+  
+  //Unlock the semaphore 
+  sem_post(&x);
+
+  printf("\n%d reader is inside",readercount);
+
+  //lock the semaphore 
+  sem_wait(&x);
+  readercount--;
+
+  if(readercount==0){
+    sem_post(&y);
+  }
+
+  //lock the semaphore
+  sem_post(&x);
+
+  printf("\n%dReader is leaving", readercount +1);
+  pthread_exit(NULL);
+
 }
 
+void* writer(void* param){
+  printf("\nWriter is trying to enter");
+
+  //Lock the semaphore
+  sem_wait(&y);
+
+  printf("\nWriter has entered");
+
+  sem_post(&y);
+
+  printf("\nWriter is leaving");
+  pthread_exit(NULL);
+
+}
+
+void readInputs(int sockfd){
+  for(;;){
+    char buff[10];
+    read(sockfd, &buff, sizeof(buff));
+    if(strcmp(buff, "Q") == 0) break;
+    printf("%s\n", buff);
+    bzero(&buff, sizeof(buff));
+
+  }
+
+}
+
+
 int main(int argc, char* argv[]){
-  int sockfd, connfd, len, max_sd, sd, activity, new_socket; 
-  int client_connect[4];
+  int sockfd, connfd, len; 
   unsigned int port = atoi(argv[1]);
   struct socket_addr servaddr, cli; 
   typedef struct sockaddr SA;
-  fd_set readfds;
+
+  
   //creating a socket and verifying if it connected
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if(sockfd == -1){
@@ -69,7 +111,9 @@ int main(int argc, char* argv[]){
   //populate struct
   servaddr.sin_family = AF_INET;
   servaddr.sin_port = htons(port);
-  
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);  
+
+
   //binding socket to ip address
   if((bind(sockfd, (SA*)&servaddr, sizeof(servaddr)))!= 0){
     printf("Socket failed binding\n");
@@ -78,75 +122,21 @@ int main(int argc, char* argv[]){
   else
     printf("Socket binded successfully!\n");
   //listening and verifing
-  if((listen(sockfd, 4))<0){
+  if((listen(sockfd, 100))!=0){
     printf("Listen failed.\n");
     exit(0);
   }
   else 
     printf("server is listening!\n");
   len = sizeof(cli);
-  puts("wating for connections...\n");
+  
   //accept data from client
-  while(true){
-    //clearing the socket set
-    FD_ZERO(&readfds);
-
-    //add master socket to set
-    FD_SET(sockfd, &readfds);
-    max_sd = sockfd;
-
-    //adding child to sockets to set 
-    for(int i =0; i < MAX_CLIENTS; i++){
-      //socket descriptor
-      sd = client_connect[i];
-
-      //if valid socket descriptor then add to read list
-      if(sd > 0)
-        FD_SET(sd , &readfds);
-      //hiighest file descriptor number, need it for the select function
-      if(sd > max_sd)
-        max_sd = sd;
-    }
-    /*wait for an activity on on of the sockets, 
-    timeout is NULL, so wait indefinitely*/
-    activity = select(sd, &readfds, NULL, NULL, NULL);
-
-    if((activity < 0) && (errno != EINTR))
-    printf("Select error!");
-
-      /*if something happened to the master socket(sockfd), then its 
-      and incoming connection */
-      if(FD_ISSET(sockfd, &readfds))
-      {
-      if ((new_socket = accept(sockfd, (struct sockaddr *)&servaddr, (socklen_t*)&len))<0)            {
-            perror("accept");
-            exit(EXIT_FAILURE);
-          }
-          char *message = "Welcome to multithread!";
-          
-          puts("Welcome message sent successfully!\n");
-
-          //add new socket to array of sockets
-          for(int i = 0; i < MAX_CLIENTS; i++){
-            if(client_connect[i] ==0){
-              client_connect[i] = new_socket;
-              printf("Adding to list of sockets as %d\n", i +1);
-
-              break;
-            }
-          }
-      }
-  }
 
   connfd = accept(sockfd, (SA*)&cli, &len);
-  if(connfd < 0){
-    printf("server accept failed.\n");
-    exit(0);
-  }
-  else 
-    printf("Server accepted data!\n");
-
-    func(connfd);
-
-    close(sockfd);
-}
+    if(connfd < 0){
+      exit(0);
+    }
+  readInputs(connfd);
+  printf("Thanks for playing!\n");
+  close(sockfd);
+} 
