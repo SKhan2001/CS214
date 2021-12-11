cb28003e3f9ebad3aac7bd0d91f11c2fa8e8fb6b
SKhan2001 <Sameerk3412@gmail.com>
2021-12-11 01:37:23 -0500

    Updated server.c

--- server.c
+++ server.c
@@ -10,11 +10,17 @@
 #include <sys/types.h>
 #include <pthread.h>
 #define MAX 4
+#define GRIDSIZE 10
 void readInputs(int);
-void readGrid(int);
+void readGrid();
 void* thread_handler(void*);
+void convertTo1D(char**, int);
 char server_grid[10][10];
 unsigned int port;
+int total_tomatoes =0;
+int total_connected =0; 
+
+
 
 struct socket_addr  { 
   uint16_t        sin_family;  /* Protocol family (always AF_INET) */ 
@@ -23,6 +29,8 @@ struct socket_addr  {
   unsigned char   sin_zero[8]; /* Pad to sizeof(struct sockaddr) */ 
 }; 
 
+
+
 void readInputs(int sockfd){
   for(;;){
     char buff[10];
@@ -30,31 +38,76 @@ void readInputs(int sockfd){
     if(strcmp(buff, "Q") == 0) break;
     printf("%s\n", buff);
     bzero(&buff, sizeof(buff));
+
   }
+
 }
 
-void readGrid(int sockfd){
-
-  read(sockfd, &server_grid, sizeof(server_grid));
+void readGrid(){
+  printf("total tomatoes -> %d\n", total_tomatoes);
   for(int i =0; i <10; i++){
     for(int j =0; j < 10; j++){
       printf("%c ", server_grid[i][j]);
     }
     printf("\n");
   }
-  bzero(&server_grid, sizeof(server_grid));
 
 
+}
+
+
+void convertTo1D(char** input, int tempsock)
+{
+    char result[GRIDSIZE*GRIDSIZE];
+    int ctr = 0;
+    for (int i = 0; i<GRIDSIZE; i++)
+    {
+        for (int j = 0; j<GRIDSIZE; j++)
+        {
+            result[ctr] = input[i][j];
+            ctr++;
+        }
+    }
+    //send(tempsock, result, sizeof(result)+1,0);
 }
 
 void* thread_handler(void* thread){
-   
-  printf("handler\n");
+      total_connected++;
+      convertTo1D(server_grid, thread);
+      printf("sending grid\n");
 
 }
 
+// get a random value in the range [0, 1]
+double rand01()
+{
+    return (double) rand() / (double) RAND_MAX;
+}
+
+void updatedgrid(){
+
+
+}
+
+void initgrid(){
+  for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            double r = rand01();
+            if (r < 0.1) {
+                server_grid[i][j] = 't';
+                total_tomatoes++;
+            }
+            else{
+                server_grid[i][j] = 'g';
+            }
+        }
+    }
+    while(total_tomatoes <1)initgrid();
+  }
 
 int main(int argc, char* argv[]){
+  initgrid();
+  readGrid();
   int sockfd, connfd, len,newsocket; 
   port = atoi(argv[1]);
   struct socket_addr servaddr, cli; 
